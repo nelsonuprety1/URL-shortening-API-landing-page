44f0a0dbfff69bae838f73c4d5a1dc680bb87306
Nelson Uprety <nelsonuprety3@gmail.com>
2023-02-07 15:23:52 +0000

    nav functionality

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const shortUrl = document.querySelector('input');
-const submitButton = document.querySelector('.btn-submit');
+// const shortUrl = document.querySelector('input');
+// const submitButton = document.querySelector('.btn-submit');
+const firstSectionContent = document.querySelector('.first-section-content');
 
-submitButton.addEventListener('click', getValue);
+// submitButton.addEventListener('click', getValue);
 
-function getValue() {
-  const url = shortUrl.value;
-  //   console.log(url);
+// function getValue() {
+//   const url = shortUrl.value;
+//   //   console.log(url);
 
-  async function fetchData() {
-    const response = await fetch(
-      `https://api.shrtco.de/v2/shorten?url=https://api.shrtco.de/v2/shorten?url=${url}`
-    );
-    const data = await response.json();
-    const html = `<h2>Shortened url</h2>
-    <p>The shortened URL is ${data.result.full_short_link}</p>
-    `;
-    document.body.innerHTML = html;
+//   async function fetchData() {
+//     const response = await fetch(
+//       `https://api.shrtco.de/v2/shorten?url=https://api.shrtco.de/v2/shorten?url=${url}`
+//     );
+//     const data = await response.json();
+//     const html = `<h2>Shortened url</h2>
+//     <p>The shortened URL is ${data.result.full_short_link}</p>
+//     `;
+//     document.body.innerHTML = html;
 
-    console.log(data.result.full_short_link);
+//     console.log(data.result.full_short_link);
+//   }
+//   fetchData();
+// }
+
+// Nav menu open and close
+function showHideMenu() {
+  let navLinks = document.getElementById('navLinks');
+  if (navLinks.style.visibility === 'visible') {
+    navLinks.style.visibility = 'hidden';
+    firstSectionContent.classList.remove('menu-open');
+  } else {
+    navLinks.style.visibility = 'visible';
+    firstSectionContent.classList.add('menu-open');
   }
-  fetchData();
 }
