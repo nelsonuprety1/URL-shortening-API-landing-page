79104bb12d26d9abe1b94e95d95e24c16085330d
Nelson Uprety <nelsonuprety3@gmail.com>
2023-02-16 01:07:06 +0000

    added error message

--- script.js
+++ script.js
@@ -52,29 +52,50 @@ hamburger.addEventListener('click', function () {
 
 // Api calls
 const shortItButton = document.querySelector('.short-it');
+
 shortItButton.addEventListener('click', function () {
+  const urlCopy = document.querySelector('.hide-url');
   const linkBox = document.getElementById('linkBox');
   const linkValue = linkBox.value;
+  const errorMessage = document.querySelector('.error-msg');
 
   const regexUrl =
     /^(?:[a-z]+:)?\/\/(?:(?:[\w$\-_.+!*'(),]|(?:%[0-9a-f]{2}))+:@)?(?:(?:[a-z0-9\-_]+\.)*[a-z0-9\-_]+|\[(?:(?:[0-9a-f]{1,4}:)*(?:[0-9a-f]{1,4})\])|\[v[a-f0-9]\.[\w$\-_.+!*'(),]*\])(?::[0-9]+)?(?:[\/|\?](?:[\w$\-_.+!*'(),;\/?:@&=+$\|#]|(?:%[0-9a-f]{2}))*)?(?:#[\w\-_.]*)?$/;
 
   const testingUrl = regexUrl.test(linkValue);
 
-  if (testingUrl === true) {
+  if (testingUrl === true && linkValue !== '') {
     try {
       async function fetchData() {
         const response = await fetch(
           `https://api.shrtco.de/v2/shorten?url=https://api.shrtco.de/v2/shorten?url=${linkValue}`
         );
         const data = await response.json();
-        console.log(data.result.full_short_link);
+
+        const html = `
+    <section class="copying-url">
+        <p class="given-link">${linkValue}</p>
+        <hr>
+        <div class="return-links">
+          <p class="small-link"><a href="${data.result.full_short_link}" target="_blank">${data.result.full_short_link}</a></p>
+          <button class="copy">Copy</button>
+        </div>
+    </section>
+        `;
+
+        urlCopy.insertAdjacentHTML('beforeend', html);
+        urlCopy.style.display = 'block';
+        // console.log(data.result.full_short_link);
+        errorMessage.style.display = 'none';
+        linkBox.style.border = 'none';
       }
       fetchData();
     } catch (error) {
       console.error('An error occurred:', error);
     }
   } else {
-    console.log('Not valid');
+    // console.log('Not valid');
+    errorMessage.style.display = 'block';
+    linkBox.style.border = '2px solid red';
   }
 });
